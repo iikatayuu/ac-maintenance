0c10406d1d1725a46f5fd96235f89027008cac1b
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2022-08-07 12:54:18 +0800

    build: add database for setup

---
+++ setup/database.sql
@@ -0,0 +1,260 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 07, 2022 at 12:50 PM
+-- Server version: 8.0.30-0ubuntu0.20.04.2
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `acmaintenance`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `offices`
+--
+
+CREATE TABLE `offices` (
+  `id` int UNSIGNED NOT NULL COMMENT 'Office ID',
+  `name` varchar(255) NOT NULL COMMENT 'Office Name',
+  `abbr` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT 'Office Abbreviations/Acronyms'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Dumping data for table `offices`
+--
+
+INSERT INTO `offices` (`id`, `name`, `abbr`) VALUES
+(1, 'Office of the Provincial Accountant', 'ACCT'),
+(2, 'Office of the Provincial Administrator', 'ADMI'),
+(3, 'Office of the Provincial Administrator (ADMP)', 'ADMP'),
+(4, 'ABANSE NEGRENSE DISTRICT ANIMAL HEALTH CENTERS', 'AND'),
+(5, 'Office of the Provincial Assessor', 'ASSE'),
+(6, 'Bids and Awards Committee Secretariat', 'BAC'),
+(7, 'BIDS & AWARDS COMMITTEE -CONSOLIDATED', 'BACC'),
+(8, 'Cauayan District Hospital', 'CAUH'),
+(9, 'Cadiz District Hospital', 'CDH'),
+(10, 'Negros Occidental Comprehensive Health Program Division', 'CHPD'),
+(11, 'Calatrava District Hospital', 'CMH'),
+(12, 'Office of the Provincial Auditor - COA', 'COA'),
+(13, 'Provincial Comelec', 'COM'),
+(14, 'Communication System', 'COMM'),
+(15, 'Civil Service Commission', 'CSC'),
+(16, 'Neg. Occ. Scholarship Program - (D.E.S.)', 'DES'),
+(17, 'Department of Interior and Local Government', 'DILG'),
+(18, 'Disaster Management Program Division', 'DMPD'),
+(19, 'Don Salvador Benedicto Memorial Hospital (La Carlota City)', 'DSBH'),
+(20, 'Eleuterio T. Decena Memorial Hospital (Hinoba-an)', 'EDMH'),
+(21, 'Electronic Data Processing Division', 'EDP'),
+(22, 'Economic Enterprises Development Department', 'EEDD'),
+(23, 'EEDD-MAMBUKAL RESORT', 'EEDD-MR'),
+(24, 'EEDD - Lodging Services', 'EEDL'),
+(25, 'EEDD - Food Terminal Market', 'FTM'),
+(26, 'EEDD- FOOD TERMINAL MARKET', 'FTM'),
+(27, 'Neg. Occ. Scholarship Program - (G.A.D.)', 'GAD'),
+(28, 'Gender and Development Programs', 'GADP'),
+(29, 'Office of the Governor', 'GOV'),
+(30, 'General Services Office', 'GSO'),
+(31, 'HOSPITAL OPERATION DEPARTMENT', 'HOD'),
+(32, 'Internal Audit Division', 'IAD'),
+(33, 'Ignacio L. Arroyo Sr. Memorial District Hospital', 'IADH'),
+(34, 'Information & Communication Technology Division', 'ICTD'),
+(35, 'Inapoy Primary Hospital', 'IPRH'),
+(36, 'Provincial Jail Management Office', 'JAIL'),
+(37, 'Luz Sikatuna Community Primary Hospital', 'LCPH'),
+(38, 'Provincial Library Services', 'LIB'),
+(39, 'Luz-Sikatuna Community Primary Hospital', 'LUZ'),
+(40, 'Lorenzo D. Zayco District Hospital', 'LZDH'),
+(41, 'Mambukal Resort', 'MAM'),
+(42, 'Merceditas Montilla District Hospital', 'MMDH'),
+(43, 'Negros First Blood Center', 'NFBC'),
+(44, 'NEGROS FIRST CYBER PARK - EEDD', 'NFCP'),
+(45, 'Negros First Cyber Park', 'NFCP'),
+(46, 'Negros Occidental Barangay Center', 'NOBC'),
+(47, 'Negros Occidental Business Development Center', 'NOBD'),
+(48, 'EEDD - NEGROS RESIDENCES', 'NOH'),
+(49, 'Negros Occidental Hostel', 'NOH'),
+(50, 'Neg. Occ. Investment & Promotion Center', 'NOIP'),
+(51, 'Neg. Occ. Language & Information Tech. Center', 'NOLI'),
+(52, 'Negros Occidental Language and I.T. Center', 'NOLI'),
+(53, 'Negros Occidental Provincial Industrial Estate Office', 'NOPI'),
+(54, 'Neg. Occ. Scholarship Program Division', 'NOSP'),
+(55, 'Negros Occidental Tourism Center', 'NOTC'),
+(56, 'Negros Occidental Tourism Division', 'NOTD'),
+(57, 'Neg. Occ. Women & Children Center', 'NOWC'),
+(58, 'Office of the Provincial Agriculturist', 'OPA'),
+(59, 'Provincial Bantay Dagat Support Program', 'OPA9'),
+(60, 'OPA-Provincial Bantay Dagat Support Program', 'OPABD'),
+(61, 'OPA-TECHNO GABAY PROGRAM IMPLEMENTATIONS-FITS', 'OPAFITS'),
+(62, 'Organic Farming Devt. & Promotions Division', 'ORG'),
+(63, 'Office of the Senior Citizens', 'OSC'),
+(64, 'Public Affairs Section', 'PAFF'),
+(65, 'Park Administration Division', 'PARK'),
+(66, 'Provincial Budget Office', 'PBO'),
+(67, 'Provincial Cooperative Office', 'PCO'),
+(68, 'Provincial Council for the Protection of Children', 'PCPC'),
+(69, 'PDC Secretariat', 'PDC'),
+(70, 'Provincial Disaster Management Program', 'PDMP'),
+(71, 'Provincial Disaster Risk Reduction and Management Council', 'PDRR'),
+(72, 'Neg. Occ. Scholarship Program - (P.E.A.C.E.)', 'PEAC'),
+(73, 'Technical Support Services Division - PEMO', 'PEMO'),
+(74, 'Provincial Environment and Management Office', 'PEMO'),
+(75, 'Provincial Mining Regulatory Board', 'PEMOA'),
+(76, 'Provincial Nursery and Arboretum', 'PEMOB'),
+(77, 'Provincial Environment Week and Other Thematic Events', 'PEMOC'),
+(78, 'Provincial Engineer\'s Office', 'PEO'),
+(79, 'Construction and Maintenance - PEO', 'PEOB'),
+(80, 'PEO - Construction & Maintenance', 'PEOC'),
+(81, 'PEO - Motor Pool', 'PEOM'),
+(82, 'PEO - Planning Design and Programming', 'PEOP'),
+(83, 'PEO - Quality Control', 'PEOQ'),
+(84, 'PEO - Rock Crusher', 'PEOR'),
+(85, 'PEO - Spring and Water Systems', 'PEOS'),
+(86, 'Public Employment Office - Neg. Occ.', 'PESO'),
+(87, 'Provincial Health Office', 'PHO'),
+(88, 'Prov\'l Human Resource Management Office', 'PHRM'),
+(89, 'Provincial Information Office Division', 'PID'),
+(90, 'Public Information Office', 'PIO'),
+(91, 'Provincial Legal Office', 'PLO'),
+(92, 'Provincial Land Use Committee', 'PLUC'),
+(93, 'Provincial Nutrition Action Plan', 'PNAP'),
+(94, 'Provincial Nutrition Committee', 'PNC'),
+(95, 'PNO - NONESCOST SCHOOL OF NURSING', 'PNSN'),
+(96, 'Provincial Planning and Development Office', 'PPDO'),
+(97, 'Provincial Project Monitoring Committee', 'PPMC'),
+(98, 'Provincial Peace and Order Council', 'PPOC'),
+(99, 'Panaad Park and Stadium Division', 'PPS'),
+(100, 'PRDP - PPMIU', 'PRDP'),
+(101, 'Programs on Awards & Incentives for Services Excellence', 'PRI'),
+(102, 'Provincial Parole and Probation Office', 'PROB'),
+(103, 'Provincial Prosecutor\'s Office', 'PROP'),
+(104, 'DECS - Provincial School Board', 'PSB'),
+(105, 'Provincial Statistical Coordinating Committee', 'PSCC'),
+(106, 'Provincial Social Welfare and Development Office', 'PSWD'),
+(107, 'Office of the Provincial Treasurer', 'PTO'),
+(108, 'Procurement and Technical Services Division', 'PTSD'),
+(109, 'Register of Deeds', 'ROD'),
+(110, 'Negros First Rice Processing Center', 'RPC'),
+(111, 'Regional Trial Court', 'RTC'),
+(112, 'Silay Oxygen Plant', 'SOP'),
+(113, 'Office of the Sangguniang Panlalawigan', 'SP'),
+(114, 'Special Programs and Concerns Division', 'SPCD'),
+(115, 'PPDO-Special Projects Division', 'SPD'),
+(116, 'Special Projects Management Office', 'SPD'),
+(117, 'Special Projects Management Unit', 'SPMU'),
+(118, 'Safety and Security Division', 'SSD'),
+(119, 'Sports Development Program Section', 'SYDO'),
+(120, 'Task Force Ilahas (20% Development Fund)', 'TASK'),
+(121, 'Office of the Governor - Task Force Ilahas', 'TFI'),
+(122, 'Teresita L. Jalandoni Provincial Hospital (Silay City)', 'TJPH'),
+(123, 'Technology and Livelihood Development Center', 'TLDC'),
+(124, 'Valladolid District Hospital', 'VDH'),
+(125, 'Office of the Provincial Veterinarian', 'VET'),
+(126, 'ABANSE NEGRENSE DISTRICT ANIMAL HEALTH CENTERS - VET', 'VETA'),
+(127, 'ABANSE NEGRENSE INTEGRATED LABORATORY - VET', 'VETB'),
+(128, 'Provincial Animal Learning Park and Hospital - VET', 'VETC'),
+(129, 'Provincial Livestock Breeding Center & Dairy Farm', 'VETD'),
+(130, 'OPERATION OF NEGROS FIRST FARM - VET', 'VETE'),
+(131, 'V. M. Gatuslao Memorial Hospital (Himamaylan)', 'VGAT'),
+(132, 'V.M. Gatuslao Memorial Hospital (Himamaylan) -', 'VGAT'),
+(133, 'Office of the Vice-Governor', 'VGOV'),
+(134, 'V. Gustilo District Hospital (Escalante)', 'VGUS');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `operations`
+--
+
+CREATE TABLE `operations` (
+  `id` int UNSIGNED NOT NULL COMMENT 'Operation ID',
+  `unit_id` int UNSIGNED NOT NULL COMMENT 'Unit ID',
+  `operation` int UNSIGNED NOT NULL COMMENT 'Operation Done: 1 - Clean, 2 - Repair, 3 - Clean and Repair',
+  `description` text NOT NULL COMMENT 'Operation description and details',
+  `date_start` datetime NOT NULL COMMENT 'Starting date of the operation',
+  `date_end` datetime NOT NULL COMMENT 'Ending date of the operation'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `units`
+--
+
+CREATE TABLE `units` (
+  `id` int UNSIGNED NOT NULL COMMENT 'AC Unit ID',
+  `office_id` int UNSIGNED NOT NULL COMMENT 'Office ID',
+  `area` varchar(255) NOT NULL COMMENT 'Office Area',
+  `type` varchar(255) NOT NULL COMMENT 'AC Unit Type',
+  `status` varchar(255) NOT NULL COMMENT 'AC Unit Status',
+  `category` varchar(255) NOT NULL COMMENT 'AC Unit Category',
+  `cooling_capacity` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT 'AC Unit Cooling Capacity (KJ/hr)',
+  `capacity_rating` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT 'AC Unit Capacity Rating (HP or TR)',
+  `energy_efficiency_ratio` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT 'Energy Efficiency Ratio (EER)',
+  `purchase_year` varchar(4) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT 'Year of Purchase',
+  `hours_per_day` int DEFAULT NULL COMMENT 'Hours per Day',
+  `days_per_week` int DEFAULT NULL COMMENT 'Days per Week',
+  `date_encoded` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Date encoded to system'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `offices`
+--
+ALTER TABLE `offices`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `operations`
+--
+ALTER TABLE `operations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `units`
+--
+ALTER TABLE `units`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `offices`
+--
+ALTER TABLE `offices`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Office ID', AUTO_INCREMENT=135;
+
+--
+-- AUTO_INCREMENT for table `operations`
+--
+ALTER TABLE `operations`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Operation ID';
+
+--
+-- AUTO_INCREMENT for table `units`
+--
+ALTER TABLE `units`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'AC Unit ID';
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
